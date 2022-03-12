1ced9be79718933db6ee33c7d04751985773b16d
9956799@gmail.com <Qqq_12345>
2022-03-12 21:13:22 +0200

    1

---
+++ src/IMovable.java
@@ -0,0 +1,21 @@
+public interface IMovable {
+    final static int interfaceStatA = 1; // _final_ static !!
+
+    IMovable INSTANCE = new IMovable() {
+        //static int interfaceStatA = 2; // ERROR
+
+        public int func() {
+            return 1;
+        }
+    };
+
+//    default IEngine.IAdditionalDevice defMethGas() {
+//        System.out.println("IMovable::gas");
+//        //a = 2; // error, final!
+//        IEngine.IAdditionalDevice ad = null;
+//        return ad;
+//    }
+//
+//    static void interfaceStatMeth() {
+//    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        IMovable iMovable = IMovable.INSTANCE;
+        iMovable.func();
+        int a = 1;
+    }
+}
