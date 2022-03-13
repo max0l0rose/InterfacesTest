90904ace94360ae048aa2907aa9e2e48d15a921a
9956799@gmail.com <Qqq_12345>
2022-03-13 13:49:57 +0200

    2

---
+++ src/ICar.java
@@ -0,0 +1,15 @@
+
+public interface ICar {
+
+    default void defMethod() {
+    }
+
+    private void defMethod2() {
+    }
+
+    static void statMethod() {
+    }
+
+    private static void statPrivMethod() {
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         IMovable iMovable = IMovable.INSTANCE;
-        iMovable.func();
+        IMovable iMovable2 = IMovable.INSTANCE;
+        assert(iMovable == iMovable2);
+        //iMovable.func(); // ERROR
         int a = 1;
     }
 }
